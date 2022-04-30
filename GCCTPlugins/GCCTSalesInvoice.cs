ff6a1e3e8418f05b2817369d3f85ef5c765d3214
Padalamma Kalla <pkalla@leveragesoft.com>
2022-04-30 16:17:54 -0400

    User Story USManga1234 Create Console app project

---
+++ GCCTPlugins/GCCTSalesInvoice.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCCTPlugins
+{
+    public class GCCTSalesInvoice : IPlugin
+
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //write code here
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
